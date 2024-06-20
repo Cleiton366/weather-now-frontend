65abd790ac67b821f0015adb5d57fb4a5ba8d46f
Cleiton <cleiton.queiroz@proton.me>
2024-06-20 19:17:38 -0300

    update: improve mobile view

--- src/components/forecast-panel.tsx
+++ src/components/forecast-panel.tsx
@@ -9,8 +9,8 @@ export default function ForecastPanel() {
 
   function setSwitchStyle(forecastOption: number) {
     if (forecastOption === 7) {
-      return forecast === 7 ? 'flex bg-[#34376d] rounded-[15px] w-20 h-10 items-center justify-center mr-1 text-white border rounded-[25px] border-[#42434e]' : 'mx-3';
-    } else return forecast === 3 ? 'flex bg-[#34376d] rounded-[15px] w-20 h-10 items-center justify-center ml-1 text-white border rounded-[25px] border-[#42434e]' : 'mx-3';
+      return forecast === 7 ? 'flex bg-[#34376d] rounded-[15px] w-20 h-10 items-center justify-center mr-1 text-white md:text-[12pt] border rounded-[25px] border-[#42434e]' : 'md:text-[12pt] mx-3';
+    } else return forecast === 3 ? 'flex bg-[#34376d] rounded-[15px] w-20 h-10 items-center justify-center ml-1 text-white md:text-[12pt] border rounded-[25px] border-[#42434e]' : 'md:text-[12pt] mx-3';
   }
 
   const forecastData = [
@@ -78,17 +78,17 @@ export default function ForecastPanel() {
 
   return (
     <div className="flex flex-col h-80 min-w-80 p-5 bg-[#2E2E38] border rounded-[15px] border-[#42434e] text-[#FFFFFF]">
-      <div className="flex justify-between">
+      <div className="flex flex-col items-center md:flex-row justify-between">
         <h1 className="font-bold text-[24pt]">Forecast</h1>
         <div
           className="flex cursor-pointer items-center justify-center h-12 bg-[#1E1F24] border rounded-[25px] border-[#42434e] text-[#FFFFFF]"
           onClick={handleForecast}
         >
           <div className="flex justify-between items-center">
-            <span className={setSwitchStyle(3)}>
+            <span className={`${setSwitchStyle(3)} text-[10pt]`}>
               3 days
             </span>
-            <span className={setSwitchStyle(7)}>
+            <span className={`${setSwitchStyle(7)} text-[10pt]`}>
               7 days
             </span>
           </div>
@@ -97,15 +97,15 @@ export default function ForecastPanel() {
       <div className="flex flex-col mt-5 gap-2 overflow-scroll">
         {
           forecastData.slice(0, forecast).map((data, i) => (
-            <div key={i} className="flex justify-between items-center p-2 px-10 bg-[#34376d] rounded-[1.2rem]">
+            <div key={i} className="flex justify-between items-center p-2 px-2 md:px-10 bg-[#34376d] rounded-[1.2rem]">
               <div className="flex items-center">
                 <img src={data.icon} alt="sun" className="w-12" />
-                <span className="font-bold text-[20pt] ml-5">{data.temperature}°
-                  <span className="font-bold text-[10pt]"> /{data.min}°</span>
+                <span className="font-bold text-[14pt] md:text-[20pt] ml-5">{data.temperature}°
+                  <span className="font-bold text-[8pt] md:text-[10pt]"> /{data.min}°</span>
                 </span>
               </div>
-              <h1 className="font-bold text-[20pt]">{data.date.slice(0, 2)}
-                <span className="font-bold text-[10pt] ml-2">{data.date.slice(2, data.date.length)}</span>
+              <h1 className="font-bold text-[14pt] md:text-[20pt]">{data.date.slice(0, 2)}
+                <span className="font-bold text-[8pt] md:text-[10pt] ml-2">{data.date.slice(2, data.date.length)}</span>
               </h1>
             </div>
           ))
