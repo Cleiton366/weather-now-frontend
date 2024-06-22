e0b7453a5a018df11a6f32ed46a6d78506704d57
Cleiton <cleiton.queiroz@proton.me>
2024-06-22 19:37:54 -0300

    update: update dashboard main page to use user context

--- src/app/(user)/dashboard/page.tsx
+++ src/app/(user)/dashboard/page.tsx
@@ -1,17 +1,11 @@
 'use client'
-import CitiesWeather from "@/components/cities-weather";
-import ForecastPanel from "@/components/forecast-panel";
-import Map from "@/components/map";
-import Navbar from "@/components/navbar";
-import OverviewPanel from "@/components/overview-panel";
-import WeatherPanel from "@/components/weather-panel";
-import User from "@/interfaces/user";
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { toast } from "@/components/ui/use-toast";
+import { UserProvider } from "@/contexts/user-context";
+import MainContainer from "@/components/main-container";
 
 export default function Dashboard() {
-  const [user, setUser] = useState<User>();
   const router = useRouter();
 
   useEffect(() => {
@@ -25,23 +19,11 @@ export default function Dashboard() {
       });
       router.push('/')
     };
-    setUser(user);
-  });
+  }, []);
 
   return (
-    <main className="flex min-h-screen flex-col">
-      <Navbar />
-      <div className="flex flex-col lg:flex-row w-full px-5 pt-3">
-        <div className="grid flex-grow p-2 gap-5 max-w-6xl">
-          <WeatherPanel />
-          <OverviewPanel />
-        </div>
-        <div className="grid flex-grow p-2 gap-5 md:min-w-[24rem]">
-          <Map />
-          <ForecastPanel />
-        </div>
-      </div>
-      <CitiesWeather />
-    </main>
+    <UserProvider>
+      <MainContainer />
+    </UserProvider>
   );
 }
