a8e87304b70a5f472880d499af6091f98a577c7d
Cleiton <cleiton.queiroz@proton.me>
2024-06-22 19:38:38 -0300

    feat: forecast-panel component integration

--- src/components/forecast-panel.tsx
+++ src/components/forecast-panel.tsx
@@ -1,80 +1,25 @@
+import { CityDTO } from "@/interfaces/city-dto";
+import GetNextEightDays from "@/util/get-next-eight-days";
 import { useEffect, useState } from "react";
 
-export default function ForecastPanel() {
+export default function ForecastPanel(props: { selectedCity: CityDTO | null }) {
   const [forecast, setForecast] = useState(3);
+  const forecastDays = GetNextEightDays({
+    day: 'numeric',
+    month: 'short',
+    weekday: 'short'
+  });
+  const { selectedCity } = props;
 
   function handleForecast() {
-    return setForecast(forecast === 3 ? 7 : 3);
+    return setForecast(forecast === 3 ? 8 : 3);
   }
 
   function setSwitchStyle(forecastOption: number) {
-    if (forecastOption === 7) {
-      return forecast === 7 ? 'flex bg-[#34376d] rounded-[15px] w-20 h-10 items-center justify-center mr-1 text-white md:text-[12pt] border rounded-[25px] border-[#42434e]' : 'md:text-[12pt] mx-3';
+    if (forecastOption === 8) {
+      return forecast === 8 ? 'flex bg-[#34376d] rounded-[15px] w-20 h-10 items-center justify-center mr-1 text-white md:text-[12pt] border rounded-[25px] border-[#42434e]' : 'md:text-[12pt] mx-3';
     } else return forecast === 3 ? 'flex bg-[#34376d] rounded-[15px] w-20 h-10 items-center justify-center ml-1 text-white md:text-[12pt] border rounded-[25px] border-[#42434e]' : 'md:text-[12pt] mx-3';
   }
-
-  const forecastData = [
-    {
-      icon: './line/animation-ready/clear-night.svg',
-      temperature: 22,
-      date: '17 Jul, Tue',
-      min: 11
-    },
-    {
-      icon: './line/animation-ready/cloudy.svg',
-      temperature: 22,
-      date: '18 Jul, Wed',
-      min: 11
-    },
-    {
-      icon: './line/animation-ready/partly-cloudy-night.svg',
-      temperature: 22,
-      date: '19 Jul, Thu',
-      min: 11
-    },
-    {
-      icon: './line/animation-ready/rain.svg',
-      temperature: 22,
-      date: '20 Jul, Fri',
-      min: 11
-    },
-    {
-      icon: './line/animation-ready/snow.svg',
-      temperature: 22,
-      date: '21 Jul, Sat',
-      min: 11
-    },
-    {
-      icon: './line/animation-ready/thunderstorm.svg',
-      temperature: 22,
-      date: '22 Jul, Sun',
-      min: 11
-    },
-    {
-      icon: './line/animation-ready/wind.svg',
-      temperature: 22,
-      date: '23 Jul, Mon',
-      min: 11
-    },
-    {
-      icon: './line/animation-ready/clear-night.svg',
-      temperature: 22,
-      date: '24 Jul, Tue',
-      min: 11
-    },
-    {
-      icon: './line/animation-ready/cloudy.svg',
-      temperature: 22,
-      date: '25 Jul, Wed',
-      min: 11
-    },
-    {
-      icon: './line/animation-ready/partly-cloudy-night.svg',
-      temperature: 22,
-      date: '26 Jul, Thu',
-      min: 11
-    },
-  ]
 
   return (
     <div className="flex flex-col h-80 min-w-80 p-5 bg-[#2E2E38] border rounded-[15px] border-[#42434e] text-[#FFFFFF]">
@@ -88,24 +33,24 @@ export default function ForecastPanel() {
             <span className={`${setSwitchStyle(3)} text-[10pt]`}>
               3 days
             </span>
-            <span className={`${setSwitchStyle(7)} text-[10pt]`}>
-              7 days
+            <span className={`${setSwitchStyle(8)} text-[10pt]`}>
+              8 days
             </span>
           </div>
         </div>
       </div>
       <div className="flex flex-col mt-5 gap-2 overflow-scroll">
         {
-          forecastData.slice(0, forecast).map((data, i) => (
+          selectedCity?.weather.daily.slice(0, forecast).map((weather, i) => (
             <div key={i} className="flex justify-between items-center md:p-2 px-2 md:px-10 bg-[#34376d] rounded-[1.2rem]">
               <div className="flex items-center">
-                <img src={data.icon} alt="sun" className="w-12" />
-                <span className="font-bold text-[14pt] md:text-[20pt] ml-5">{data.temperature}°
-                  <span className="font-bold text-[8pt] md:text-[10pt]"> /{data.min}°</span>
+                <img src='./line/animation-ready/clear-night.svg' alt="sun" className="w-12" />
+                <span className="font-bold text-[14pt] md:text-[20pt] ml-5">{weather.temp.day.toFixed(0)}°
+                  <span className="font-bold text-[8pt] md:text-[10pt]"> /{weather.temp.min.toFixed(0)}°</span>
                 </span>
               </div>
-              <h1 className="font-bold text-[14pt] md:text-[20pt]">{data.date.slice(0, 2)}
-                <span className="font-bold text-[8pt] md:text-[10pt] ml-2">{data.date.slice(2, data.date.length)}</span>
+              <h1 className="font-bold text-[14pt] md:text-[20pt]">{forecastDays[i].slice(0, 3)}
+                <span className="font-bold text-[8pt] md:text-[10pt] ml-2">{forecastDays[i].slice(3, forecastDays[i].length)}</span>
               </h1>
             </div>
           ))
