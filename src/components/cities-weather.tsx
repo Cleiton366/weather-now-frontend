f00c736266b4b949b260bf01b782abed375640ef
Cleiton <cleiton.queiroz@proton.me>
2024-06-23 03:04:38 -0300

    update: update cities-weather component style for mobile

--- src/components/cities-weather.tsx
+++ src/components/cities-weather.tsx
@@ -9,7 +9,7 @@ import { CityDTO } from "@/interfaces/city-dto";
 import Autoplay from "embla-carousel-autoplay"
 import { Dispatch, SetStateAction, useEffect, useState } from "react";
 
-export default function CitiesWeather(props: { cities: CityDTO[] | [], setSelectedCity: Dispatch<SetStateAction<CityDTO | null | undefined>>}) {
+export default function CitiesWeather(props: { cities: CityDTO[] | [], setSelectedCity: Dispatch<SetStateAction<CityDTO | null | undefined>> }) {
   const [citiesMatrix, setCitiesMatrix] = useState<CityDTO[][]>([[]]);
   const { cities, setSelectedCity } = props;
 
@@ -30,8 +30,9 @@ export default function CitiesWeather(props: { cities: CityDTO[] | [], setSelect
   }
 
   return (
-    <div className="w-full px-20">
+    <div className="w-full px-6 md:px-20">
       <Carousel
+        className="h-0 w-0 md:h-full md:w-full"
         plugins={[
           Autoplay({
             delay: 10000,
@@ -52,8 +53,8 @@ export default function CitiesWeather(props: { cities: CityDTO[] | [], setSelect
                   {
                     citiesRow.map((city, k) => (
                       city ? (
-                        <div 
-                          key={k} 
+                        <div
+                          key={k}
                           onClick={() => setSelectedCity(city)}
                           className="flex flex-col h-40 min-w-40 cursor-pointer items-center justify-center bg-[#2E2E38] border rounded-[15px] border-[#42434e] text-[#FFFFFF]">
                           <div className="bg-[#34376d] rounded-full border border-[#42434e] text-[#FFFFFF] p-2 mt-[-3.5rem]">
@@ -74,9 +75,38 @@ export default function CitiesWeather(props: { cities: CityDTO[] | [], setSelect
             ))
           }
         </CarouselContent>
-        <CarouselPrevious />
-        <CarouselNext />
+        <CarouselPrevious className="h-0 w-0 md:h-fit md:w-fit" />
+        <CarouselNext className="h-0 w-0 md:h-fit md:w-fit" />
       </Carousel>
+
+      <div className="h-full w-full md:hidden">
+        <div className="flex pt-10 pb-2 overflow-scroll gap-3">
+          <div className="flex flex-col h-40 min-w-40 text-center items-center justify-center border-2 border-dashed rounded-[15px] border-white/40 text-[#FFFFFF]">
+            <div className="bg-[#34376d] rounded-full border border-[#42434e] text-[#FFFFFF] p-2 mt-[-3rem]">
+              <img className="w-11" src='./line/animation-ready/falling-stars.svg' alt="" />
+            </div>
+            <h1 className="font-bold text-[12pt] w-32 mt-2 text-center">World Forecast</h1>
+            <span className="font-bold text-[9pt] mt-8 w-28">Add the cities you are interested in</span>
+          </div>
+          {
+            cities.map((city, k) => (
+              <div
+                key={k}
+                onClick={() => setSelectedCity(city)}
+                className="flex flex-col h-40 min-w-40 cursor-pointer items-center justify-center bg-[#2E2E38] border rounded-[15px] border-[#42434e] text-[#FFFFFF]">
+                <div className="bg-[#34376d] rounded-full border border-[#42434e] text-[#FFFFFF] p-2 mt-[-3.5rem]">
+                  <img className="w-11" src={`./line/animation-ready/${city.weather.current.weather[0].icon}.svg`} alt="" />
+                </div>
+                <h1 className="font-bold text-[14pt]">{city.name}</h1>
+                <span className="font-bold text-[9pt]">{city.country}</span>
+                <span className="font-bold text-[16pt] mt-5">{city.weather.current.temp.toFixed(0)}°
+                  <span className="font-bold text-[10pt]"> /{city.weather.daily[0].temp.min.toFixed(0)}°</span>
+                </span>
+              </div>
+            ))
+          }
+        </div>
+      </div>
     </div>
   )
 }
