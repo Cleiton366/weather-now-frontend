5a6ebe8a3e5beb325d29840d8f951e064ed8232b
Cleiton <cleiton.queiroz@proton.me>
2024-06-30 01:51:04 -0300

    update: add scroll area to cities weather component carousel for cross browser style

--- src/components/cities-weather.tsx
+++ src/components/cities-weather.tsx
@@ -43,35 +43,38 @@ export default function CitiesWeather(props: { cities: CityDTO[] | [], setSelect
           {
             citiesMatrix.map((citiesRow, i) => (
               <CarouselItem key={i}>
-                <div className="flex px-7 pt-10 pb-2 overflow-scroll gap-3">
-                  <div className="flex flex-col h-40 min-w-40 text-center items-center justify-center border-2 border-dashed rounded-[15px] border-white/40 text-[#FFFFFF]">
-                    <div className="bg-[#34376d] rounded-full border border-[#42434e] text-[#FFFFFF] p-2 mt-[-3rem]">
-                      <img className="w-11" src='./line/animation-ready/falling-stars.svg' alt="" />
+                <ScrollArea className="h-full w-full">
+                  <div className="flex px-7 pt-10 pb-2 gap-3">
+                    <div className="flex flex-col h-40 min-w-40 text-center items-center justify-center border-2 border-dashed rounded-[15px] border-white/40 text-[#FFFFFF]">
+                      <div className="bg-[#34376d] rounded-full border border-[#42434e] text-[#FFFFFF] p-2 mt-[-3rem]">
+                        <img className="w-11" src='./line/animation-ready/falling-stars.svg' alt="" />
+                      </div>
+                      <h1 className="font-bold text-[12pt] w-32 mt-2 text-center">World Forecast</h1>
+                      <span className="font-bold text-[9pt] mt-8 w-28">Add the cities you are interested in</span>
                     </div>
-                    <h1 className="font-bold text-[12pt] w-32 mt-2 text-center">World Forecast</h1>
-                    <span className="font-bold text-[9pt] mt-8 w-28">Add the cities you are interested in</span>
-                  </div>
-                  {
-                    citiesRow.map((city, k) => (
-                      city ? (
-                        <div
-                          key={k}
-                          onClick={() => setSelectedCity(city)}
-                          className="flex flex-col h-40 min-w-40 cursor-pointer items-center justify-center bg-[#2E2E38] border rounded-[15px] border-[#42434e] text-[#FFFFFF]">
-                          <div className="bg-[#34376d] rounded-full border border-[#42434e] text-[#FFFFFF] p-2 mt-[-3.5rem]">
-                            <img className="w-11" src={`./line/animation-ready/${city.weather.current.weather[0].icon}.svg`} alt="" />
+                    {
+                      citiesRow.map((city, k) => (
+                        city ? (
+                          <div
+                            key={k}
+                            onClick={() => setSelectedCity(city)}
+                            className="flex flex-col h-40 min-w-40 cursor-pointer items-center justify-center bg-[#2E2E38] border rounded-[15px] border-[#42434e] text-[#FFFFFF]">
+                            <div className="bg-[#34376d] rounded-full border border-[#42434e] text-[#FFFFFF] p-2 mt-[-3.5rem]">
+                              <img className="w-11" src={`./line/animation-ready/${city.weather.current.weather[0].icon}.svg`} alt="" />
+                            </div>
+                            <h1 className="font-bold text-[14pt]">{city.name}</h1>
+                            <span className="font-bold text-[9pt]">{city.country}</span>
+                            <span className="font-bold text-[16pt] mt-5">{city.weather.current.temp.toFixed(0)}°
+                              <span className="font-bold text-[10pt]"> /{city.weather.daily[0].temp.min.toFixed(0)}°</span>
+                            </span>
                           </div>
-                          <h1 className="font-bold text-[14pt]">{city.name}</h1>
-                          <span className="font-bold text-[9pt]">{city.country}</span>
-                          <span className="font-bold text-[16pt] mt-5">{city.weather.current.temp.toFixed(0)}°
-                            <span className="font-bold text-[10pt]"> /{city.weather.daily[0].temp.min.toFixed(0)}°</span>
-                          </span>
-                        </div>
-                      ) :
-                        (<div key={k} />)
-                    ))
-                  }
-                </div>
+                        ) :
+                          (<div key={k} />)
+                      ))
+                    }
+                  </div>
+                  <ScrollBar orientation="horizontal" />
+                </ScrollArea>
               </CarouselItem>
             ))
           }
