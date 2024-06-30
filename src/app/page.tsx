4743c2a0824f912905ac89d540c1ab9567101f39
Cleiton <cleiton.queiroz@proton.me>
2024-06-30 00:49:28 -0300

    fix: fix style of home page on mobile view

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,13 +8,13 @@ export default function Home() {
   const router = useRouter();
 
   return (
-    <main className="flex min-h-screen flex-col mt-5 md:justify-center">
-      <div className="flex flex-col md:flex-row justify-center items-center">
+    <main className="flex min-h-screen flex-col md:mt-5 md:justify-center">
+      <div className="flex flex-col md:flex-row  min-h-screen justify-center items-center">
         <img className="w-44 md:w-64" src="./fill/animation-ready/01d.svg" alt="" />
         <div className="mt-10 md:ml-10">
           <h1 className="px-10 md:w-[30rem] font-bold text-[16pt] md:text-[20pt]">Do you need a umbrela or sunscreens tomorrow?</h1>
           <h1 className="mt-5 px-10 md:w-[25rem] font-medium text-[12pt] md:text-[16pt]">Find out with Weather
-            <span className="bg-clip-text text-transparent bg-gradient-to-b from-[#FBFBFB] to-[#34376D]"> Now </span>
+            <span className="bg-clip-text text-transparent bg-gradient-to-b from-[#FBFBFB] to-[#8a8dd6]"> Now </span>
             Your daily weather guide!</h1>
           <div className="flex justify-center mt-10 md:justify-start md:ml-10">
           <Button
