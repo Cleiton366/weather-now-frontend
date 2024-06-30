560890dde80d05dd24e6b86120ac84bc080b7365
Cleiton <cleiton.queiroz@proton.me>
2024-06-30 02:04:18 -0300

    update: add scroll area to forecast panel component for cross browser style

--- src/components/forecast-panel.tsx
+++ src/components/forecast-panel.tsx
@@ -1,6 +1,7 @@
 import { CityDTO } from "@/interfaces/city-dto";
 import GetNextEightDays from "@/util/get-next-eight-days";
 import { useEffect, useState } from "react";
+import { ScrollArea } from "./ui/scroll-area";
 
 export default function ForecastPanel(props: { selectedCity: CityDTO | null }) {
   const [forecast, setForecast] = useState(3);
@@ -40,21 +41,23 @@ export default function ForecastPanel(props: { selectedCity: CityDTO | null }) {
         </div>
       </div>
       <div className="flex flex-col mt-5 gap-2 overflow-scroll">
-        {
-          selectedCity?.weather.daily.slice(0, forecast).map((weather, i) => (
-            <div key={i} className="flex justify-between items-center md:p-2 px-2 md:px-10 bg-[#34376d] rounded-[1.2rem]">
-              <div className="flex items-center">
-                <img src={`./line/animation-ready/${weather.weather[0].icon}.svg`} alt="sun" className="w-12" />
-                <span className="font-bold text-[14pt] md:text-[20pt] ml-5">{weather.temp.day.toFixed(0)}°
-                  <span className="font-bold text-[8pt] md:text-[10pt]"> /{weather.temp.min.toFixed(0)}°</span>
-                </span>
+        <ScrollArea className="h-full w-full">
+          {
+            selectedCity?.weather.daily.slice(0, forecast).map((weather, i) => (
+              <div key={i} className="flex justify-between items-center md:p-2 px-2 md:px-10 bg-[#34376d] rounded-[1.2rem]">
+                <div className="flex items-center">
+                  <img src={`./line/animation-ready/${weather.weather[0].icon}.svg`} alt="sun" className="w-12" />
+                  <span className="font-bold text-[14pt] md:text-[20pt] ml-5">{weather.temp.day.toFixed(0)}°
+                    <span className="font-bold text-[8pt] md:text-[10pt]"> /{weather.temp.min.toFixed(0)}°</span>
+                  </span>
+                </div>
+                <h1 className="font-bold text-[14pt] md:text-[20pt]">{forecastDays[i].slice(0, 3)}
+                  <span className="font-bold text-[8pt] md:text-[10pt] ml-2">{forecastDays[i].slice(3, forecastDays[i].length)}</span>
+                </h1>
               </div>
-              <h1 className="font-bold text-[14pt] md:text-[20pt]">{forecastDays[i].slice(0, 3)}
-                <span className="font-bold text-[8pt] md:text-[10pt] ml-2">{forecastDays[i].slice(3, forecastDays[i].length)}</span>
-              </h1>
-            </div>
-          ))
-        }
+            ))
+          }
+        </ScrollArea>
       </div>
     </div>
   )
